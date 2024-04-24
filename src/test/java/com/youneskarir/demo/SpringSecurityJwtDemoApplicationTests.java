6188a27b01afae4df6029e8e90ddd51f7bd70603
younes-karir <youneskarir@outlook.com>
2024-04-24 19:55:01 +0100

    first commit

---
+++ src/test/java/com/youneskarir/demo/SpringSecurityJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youneskarir.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
