1bfcfd96f74443cc3bc212497e3d708aa973830b
younes-karir <youneskarir@outlook.com>
2024-04-30 19:36:38 +0100

    updating the application config  create auditorAware Bean.

--- src/main/java/com/youneskarir/demo/config/ApplicationConfig.java
+++ src/main/java/com/youneskarir/demo/config/ApplicationConfig.java
@@ -1,10 +1,12 @@
 package com.youneskarir.demo.config;
 
 
+import com.youneskarir.demo.auditing.ApplicationAuditAware;
 import com.youneskarir.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -48,4 +50,9 @@ public class ApplicationConfig {
     }
 
 
+    @Bean
+    public AuditorAware<Integer> auditorAware(){
+        return new ApplicationAuditAware();
+    } 
+    
 }
