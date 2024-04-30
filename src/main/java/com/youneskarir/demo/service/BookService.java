620d53a45e34aac955b8fae206066722219cd478
younes-karir <youneskarir@outlook.com>
2024-04-30 19:37:45 +0100

    creating Book Service

---
+++ src/main/java/com/youneskarir/demo/service/BookService.java
@@ -0,0 +1,29 @@
+package com.youneskarir.demo.service;
+
+import com.youneskarir.demo.dto.BookRequest;
+import com.youneskarir.demo.model.Book;
+import com.youneskarir.demo.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository repository;
+
+    public void save(BookRequest request) {
+        var book = Book.builder()
+                .id(request.getId())
+                .author(request.getAuthor())
+                .isbn(request.getIsbn())
+                .build();
+        repository.save(book);
+    }
+
+    public List<Book> findAll() {
+        return repository.findAll();
+    }
+}
