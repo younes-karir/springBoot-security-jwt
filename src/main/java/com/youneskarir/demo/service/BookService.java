e163494d231a078b9f035ca54d5e9b23b4487699
youneskarir <youneskarir@gmail.com>
2024-05-10 20:52:42 +0100

    update Book service

--- src/main/java/com/youneskarir/demo/service/BookService.java
+++ src/main/java/com/youneskarir/demo/service/BookService.java
@@ -14,13 +14,14 @@ public class BookService {
 
     private final BookRepository repository;
 
-    public void save(BookRequest request) {
+    public Book save(BookRequest request) {
         var book = Book.builder()
                 .id(request.getId())
                 .author(request.getAuthor())
                 .isbn(request.getIsbn())
                 .build();
         repository.save(book);
+        return book;
     }
 
     public List<Book> findAll() {
