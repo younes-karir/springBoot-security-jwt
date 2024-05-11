5324887a0d6c6557d3ea09d21834d43c29d6f9cd
youneskarir <youneskarir@gmail.com>
2024-05-11 17:48:16 +0100

    creating a custom enum validator

---
+++ src/main/java/com/youneskarir/demo/validation/EnumNamePatternValidator.java
@@ -0,0 +1,31 @@
+package com.youneskarir.demo.validation;
+
+import com.youneskarir.demo.advice.custom.NotValidEnumValueException;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class EnumNamePatternValidator implements ConstraintValidator<EnumNamePattern, Enum<?>> {
+    private Pattern pattern;
+
+    @Override
+    public void initialize(EnumNamePattern annotation) {
+        try {
+            pattern = Pattern.compile(annotation.regexp());
+        } catch (PatternSyntaxException e) {
+            throw new NotValidEnumValueException("Given regex is invalid");
+        }
+    }
+
+    @Override
+    public boolean isValid(Enum<?> value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+        Matcher m = pattern.matcher(value.name());
+        return m.matches();
+    }
+}
